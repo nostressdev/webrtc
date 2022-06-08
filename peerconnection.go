eda9b292fd67e68dba6ddfd1b6c0f6b4610a224d
Alisa Vernigor <vernigor.alisa@gmail.com>
2022-06-08 23:11:40 +0300

    add createoffer

--- peerconnection.go
+++ peerconnection.go
@@ -38,6 +38,11 @@ type RTCPeerConnection struct {
 	// TODO(@alisa-vernigor): add event handlers
 }
 
+type sessionDescOptions struct {
+	includeRTCPSize   bool
+	includeBundleOnly bool
+}
+
 func (pc *RTCPeerConnection) isBundleOnly(bundlePolicy RTCBundlePolicy, isFirstInGroup map[string]bool, kind string) bool {
 	if bundlePolicy == RTCBundlePolicyBalancded {
 		isFirst := isFirstInGroup[kind]
@@ -76,108 +81,80 @@ func randStringWithCharset(length int, charset string) string {
 	return string(b)
 }
 
-func (pc *RTCPeerConnection) generateInitialOffer() (*sdp.Session, error) {
-	session := &sdp.Session{}
-	session.Version = 0
-	session.Originator = &sdp.Origin{
-		Username:       "-",
-		SessID:         rand.Int63(),
-		SessVersion:    pc.sessVersion,
-		Nettype:        sdp.NetworkInternet,
-		Addrtype:       sdp.TypeIPv4,
-		UnicastAddress: "0.0.0.0",
+func (pc *RTCPeerConnection) addAudioCodecs(media *sdp.MediaDesc) {
+	media.Fmts = []string{"35"}
+	media.AddAttribute("rtpmap", "35 opus/48000")
+	media.AddAttribute("fmtp", "35 useinbandfec=1")
+}
+
+func (pc *RTCPeerConnection) addVideoCodecs(media *sdp.MediaDesc) {
+	media.Fmts = []string{"36"}
+	media.AddAttribute("rtpmap", "36 H264 AVC/90000")
+	media.AddAttribute("maxptime", "120")
+}
+
+func (pc *RTCPeerConnection) addMediaSection(session *sdp.Session, tranceiver *RTCRtpTransceiver,
+	mids []string, msidsToMidStrings map[string][]string, isFirstInGroup map[string]bool, tlsID string,
+	iceUfrag string, icePwd string, fingerprints []*RTCDtlsFingerprint) {
+	if tranceiver.stopped || tranceiver.stopping {
+		return
 	}
-	session.SessionName = "-"
-	session.Timings = []*sdp.Timing{
+
+	bundlePolicy := pc.configuration.BundlePolicy
+	media := &sdp.MediaDesc{}
+	kind := tranceiver.receiver.track.kind
+	media.Media = kind
+
+	if tranceiver.sender.track.kind == "audio" {
+		pc.addAudioCodecs(media)
+	} else if tranceiver.sender.track.kind == "video" {
+		pc.addVideoCodecs(media)
+	}
+
+	if pc.isBundleOnly(bundlePolicy, isFirstInGroup, kind) {
+		media.Port = 0
+		media.AddAttribute("bundle-only", " ")
+	} else {
+		media.Port = 9
+		media.AddAttribute("rtcp-rsize", " ")
+		media.AddAttribute("setup", "actpass")
+		media.AddAttribute("tls-id", tlsID)
+		media.AddAttribute("ice-ufrag", iceUfrag)
+		media.AddAttribute("ice-pwd", icePwd)
+
+		for _, fingerprint := range fingerprints {
+			media.AddAttribute("fingerprint", fingerprint.Algorithm+" "+fingerprint.Value)
+		}
+	}
+	media.Proto = []string{"UDP", "TLS", "RTP", "SAVPF"}
+
+	for _, mediaStreamId := range tranceiver.sender.associatedMediaStreamIds {
+		media.AddAttribute("msid", mediaStreamId)
+		msidsToMidStrings[mediaStreamId] = append(msidsToMidStrings[mediaStreamId], strconv.FormatInt(pc.midCounter, 10))
+	}
+	media.Connections = []*sdp.Connection{
 		{
-			Start: 0,
-			Stop:  0,
+			Nettype:        sdp.NetworkInternet,
+			Addrtype:       sdp.TypeIPv4,
+			ConnectionAddr: "0.0.0.0",
+			AddressesNum:   1,
 		},
 	}
 
-	session.AddAttribute("ice-options", "trickle ice2")
+	mid := strconv.FormatInt(pc.midCounter, 10)
+	tranceiver.mid = mid
+	media.AddAttribute("mid", mid)
+	mids = append(mids, mid)
+	pc.midCounter++
 
-	mids := []string{}
-	msidsToMidStrings := make(map[string][]string)
+	media.AddAttribute(string(tranceiver.direction), " ")
 
-	bundlePolicy := pc.configuration.BundlePolicy
-	isFirstInGroup := map[string]bool{
-		"audio":       true,
-		"video":       true,
-		"text":        true,
-		"application": true,
-		"message":     true,
-	}
+	session.MediaDescs = append(session.MediaDescs, media)
+}
 
-	tlsID := randStringWithCharset(120, alphaNumCharset)
-	iceUfrag := randStringWithCharset(4, alphaNumCharset)
-	icePwd := randStringWithCharset(22, alphaNumCharset)
-	var fingerprints []*RTCDtlsFingerprint
-	var err error
-	for _, cert := range pc.configuration.Certificates {
-		fingerprints, err = cert.getFingerprints()
-		if err != nil {
-			return nil, fmt.Errorf("error while getting fingerprints: %v", err)
-		}
-	}
-
-	for _, tranceiver := range pc.rtpTransceivers {
-		if tranceiver.stopped || tranceiver.stopping {
-			continue
-		}
-		media := &sdp.MediaDesc{}
-		kind := tranceiver.receiver.track.kind
-
-		media.Media = kind
-
-		if tranceiver.sender.track.kind == "audio" {
-			media.Fmts = []string{"35"}
-			media.AddAttribute("rtpmap", "35 opus/48000")
-			media.AddAttribute("fmtp", "35 useinbandfec=1") // Specifies that the decoder has the capability to take advantage of the Opus in-band FEC (Forward Error Correction)
-		} else if tranceiver.sender.track.kind == "video" {
-			media.Fmts = []string{"36"}
-			media.AddAttribute("rtpmap", "36 H264 AVC/90000")
-			media.AddAttribute("maxptime", "120")
-		}
-
-		if pc.isBundleOnly(bundlePolicy, isFirstInGroup, kind) {
-			media.Port = 0
-			media.AddAttribute("bundle-only", " ")
-		} else {
-			media.Port = 9
-			media.AddAttribute("rtcp-rsize", " ")
-			media.AddAttribute("setup", "actpass")
-			media.AddAttribute("tls-id", tlsID)
-			media.AddAttribute("ice-ufrag", iceUfrag)
-			media.AddAttribute("ice-pwd", icePwd)
-
-			for _, fingerprint := range fingerprints {
-				media.AddAttribute("fingerprint", fingerprint.Algorithm+" "+fingerprint.Value)
-			}
-		}
-		media.Proto = []string{"UDP", "TLS", "RTP", "SAVPF"}
-
-		for _, mediaStreamId := range tranceiver.sender.associatedMediaStreamIds {
-			media.AddAttribute("msid", mediaStreamId)
-			msidsToMidStrings[mediaStreamId] = append(msidsToMidStrings[mediaStreamId], strconv.FormatInt(pc.midCounter, 10))
-		}
-		media.Connections = []*sdp.Connection{
-			{
-				Nettype:        sdp.NetworkInternet,
-				Addrtype:       sdp.TypeIPv4,
-				ConnectionAddr: "0.0.0.0",
-				AddressesNum:   1,
-			},
-		}
-
-		mid := strconv.FormatInt(pc.midCounter, 10)
-		media.AddAttribute("mid", mid)
-		mids = append(mids, mid)
-		pc.midCounter++
-
-		media.AddAttribute(string(tranceiver.direction), " ")
-	}
-
+func (pc *RTCPeerConnection) addDataMediaSection(session *sdp.Session,
+	mids []string, msidsToMidStrings map[string][]string, isFirstInGroup map[string]bool, tlsID string,
+	iceUfrag string, icePwd string, fingerprints []*RTCDtlsFingerprint) {
 	media := &sdp.MediaDesc{}
 	media.Connections = []*sdp.Connection{
 		{
@@ -207,6 +184,69 @@ func (pc *RTCPeerConnection) generateInitialOffer() (*sdp.Session, error) {
 	mids = append(mids, mid)
 	pc.midCounter++
 
+	session.MediaDescs = append(session.MediaDescs, media)
+}
+
+func (pc *RTCPeerConnection) addMediaSections(session *sdp.Session, mids []string, msidsToMidStrings map[string][]string) error {
+	isFirstInGroup := map[string]bool{
+		"audio":       true,
+		"video":       true,
+		"text":        true,
+		"application": true,
+		"message":     true,
+	}
+
+	tlsID := randStringWithCharset(120, alphaNumCharset)
+	iceUfrag := randStringWithCharset(4, alphaNumCharset)
+	icePwd := randStringWithCharset(22, alphaNumCharset)
+	var fingerprints []*RTCDtlsFingerprint
+	var err error
+	for _, cert := range pc.configuration.Certificates {
+		fingerprints, err = cert.getFingerprints()
+		if err != nil {
+			return fmt.Errorf("error while getting fingerprints: %v", err)
+		}
+	}
+
+	for _, tranceiver := range pc.rtpTransceivers {
+		pc.addMediaSection(session, tranceiver, mids, msidsToMidStrings, isFirstInGroup, tlsID, iceUfrag, icePwd, fingerprints)
+	}
+
+	pc.addDataMediaSection(session, mids, msidsToMidStrings, isFirstInGroup, tlsID, iceUfrag, icePwd, fingerprints)
+
+	return nil
+}
+
+func (pc *RTCPeerConnection) createSessionDesc() (*sdp.Session, error) {
+	session := &sdp.Session{}
+	session.Version = 0
+	session.Originator = &sdp.Origin{
+		Username:       "-",
+		SessID:         rand.Int63(),
+		SessVersion:    pc.sessVersion,
+		Nettype:        sdp.NetworkInternet,
+		Addrtype:       sdp.TypeIPv4,
+		UnicastAddress: "0.0.0.0",
+	}
+	pc.sessVersion += 1
+
+	session.SessionName = "-"
+	session.Timings = []*sdp.Timing{
+		{
+			Start: 0,
+			Stop:  0,
+		},
+	}
+
+	session.AddAttribute("ice-options", "trickle ice2")
+	mids := []string{}
+	msidsToMidStrings := make(map[string][]string)
+
+	err := pc.addMediaSections(session, mids, msidsToMidStrings)
+	if err != nil {
+		return nil, fmt.Errorf("error while adding mediasections: %v", err)
+	}
+
 	if len(mids) > 0 {
 		session.AddAttribute("group", "BUNDLE "+strings.Join(mids, " "))
 	}
@@ -220,6 +260,17 @@ func (pc *RTCPeerConnection) generateInitialOffer() (*sdp.Session, error) {
 	return session, nil
 }
 
+func (pc *RTCPeerConnection) createSubsequentOffer() (*sdp.Session, error) {
+	if pc.signalingState == RTCSignalingStateStable {
+		return pc.createSessionDesc()
+	}
+
+	if pc.currentRemoteDescription != nil {
+	}
+
+	return nil, nil
+}
+
 func (pc *RTCPeerConnection) CreateOffer(options *RTCOfferOptions) (*RTCSessionDescription, error) {
 	if pc.isClosed {
 		return nil, makeError(ErrInvalidState, "connection is closed")
@@ -229,6 +280,7 @@ func (pc *RTCPeerConnection) CreateOffer(options *RTCOfferOptions) (*RTCSessionD
 		return nil, makeError(ErrInvalidState, "connection's signaling state is neither stable nor have-local-offer")
 	}
 
+	var offer *RTCSessionDescription
 	for {
 		pc.certsGenerated.L.Lock()
 		for len(pc.configuration.Certificates) == 0 {
@@ -239,5 +291,21 @@ func (pc *RTCPeerConnection) CreateOffer(options *RTCOfferOptions) (*RTCSessionD
 		// TODO(@alisa-vernigor): Inspect the offerer's system state to determine the currently available
 		// resources as necessary for generating the offer, as described in [RFC8829] (section 4.1.8.)
 
+		offer = &RTCSessionDescription{}
+		offer.SDPType = RTCSdpTypeOffer
+
+		if pc.lastCreatedOffer == "" {
+			session, err := pc.createSessionDesc()
+			if err != nil {
+				return nil, fmt.Errorf("error while creating offer: %v", err)
+			} else {
+				offer.setSession(session)
+				break
+			}
+		} else {
+
+		}
 	}
+
+	return offer, nil
 }
--- sdp/marshal_test.go
+++ sdp/marshal_test.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+	gosdp "github.com/pixelbender/go-sdp/sdp"
 )
 
 var marshalTests = []*testVector{
@@ -274,4 +276,232 @@ func FuzzEncode(f *testing.F) {
 			}
 		}
 	})
+}
+
+func parseTime(s string) time.Time {
+	t, err := time.Parse("2006-01-02 15:04:05 -0700 MST", s)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+var defaultMarshalTests = []*defaultTestVector{
+	{
+		Name: "RFC4566 Example",
+		Data: `v=0
+o=jdoe 2890844526 2890842807 IN IP4 10.47.16.5
+s=SDP Seminar
+i=A Seminar on the session description protocol
+u=http://www.example.com/seminars/sdp.pdf
+e=j.doe@example.com (Jane Doe)
+p=+1 617 555-6011
+c=IN IP4 224.2.17.12/127
+b=AS:2000
+t=3034423619 3042462419
+r=7d 1h 0 25h
+z=3034423619 -1h 3042462419 0
+a=recvonly
+m=audio 49170 RTP/AVP 0
+m=video 51372 RTP/AVP 99 100
+a=rtpmap:99 h263-1998/90000
+a=rtpmap:100 H264/90000
+a=rtcp-fb:100 ccm fir
+a=rtcp-fb:100 nack
+a=rtcp-fb:100 nack pli
+a=fmtp:100 profile-level-id=42c01f;level-asymmetry-allowed=1
+`,
+		Session: &gosdp.Session{
+			Origin: &gosdp.Origin{
+				Username:       "jdoe",
+				SessionID:      2890844526,
+				SessionVersion: 2890842807,
+				Network:        NetworkInternet,
+				Type:           TypeIPv4,
+				Address:        "10.47.16.5",
+			},
+			Name:        "SDP Seminar",
+			Information: "A Seminar on the session description protocol",
+			URI:         "http://www.example.com/seminars/sdp.pdf",
+			Email:       []string{"j.doe@example.com (Jane Doe)"},
+			Phone:       []string{"+1 617 555-6011"},
+			Connection: &gosdp.Connection{
+				Network: NetworkInternet,
+				Type:    TypeIPv4,
+				Address: "224.2.17.12",
+				TTL:     127,
+			},
+			Bandwidth: []*gosdp.Bandwidth{
+				{"AS", 2000},
+			},
+			Timing: &gosdp.Timing{
+				Start: parseTime("1996-02-27 15:26:59 +0000 UTC"),
+				Stop:  parseTime("1996-05-30 16:26:59 +0000 UTC"),
+			},
+			Repeat: []*gosdp.Repeat{
+				{
+					Interval: time.Duration(604800) * time.Second,
+					Duration: time.Duration(3600) * time.Second,
+					Offsets: []time.Duration{
+						time.Duration(0),
+						time.Duration(90000) * time.Second,
+					},
+				},
+			},
+			TimeZone: []*gosdp.TimeZone{
+				{Time: parseTime("1996-02-27 15:26:59 +0000 UTC"), Offset: -time.Hour},
+				{Time: parseTime("1996-05-30 16:26:59 +0000 UTC"), Offset: 0},
+			},
+			Media: []*gosdp.Media{
+				{
+					Type:  "audio",
+					Port:  49170,
+					Proto: "RTP/AVP",
+					Format: []*gosdp.Format{
+						{Payload: 0},
+					},
+				},
+				{
+					Type:  "video",
+					Port:  51372,
+					Proto: "RTP/AVP",
+					Format: []*gosdp.Format{
+						{Payload: 99, Name: "h263-1998", ClockRate: 90000},
+						{Payload: 100, Name: "H264", ClockRate: 90000, Params: []string{
+							"profile-level-id=42c01f;level-asymmetry-allowed=1",
+						}, Feedback: []string{
+							"ccm fir", "nack", "nack pli",
+						}},
+					},
+				},
+			},
+			Attributes: []*gosdp.Attr{{
+				Name:  "recvonly",
+				Value: " ",
+			},
+			},
+		},
+	},
+	{
+		Name: "Readme Example",
+		Data: `v=0
+o=alice 2890844526 2890844526 IN IP4 alice.example.org
+s=Example
+c=IN IP4 127.0.0.1
+t=0 0
+a=sendrecv
+m=audio 10000 RTP/AVP 0 8
+a=rtpmap:0 PCMU/8000
+a=rtpmap:8 PCMA/8000
+`,
+		Session: &gosdp.Session{
+			Origin: &gosdp.Origin{
+				Username:       "alice",
+				SessionID:      2890844526,
+				SessionVersion: 2890844526,
+				Network:        NetworkInternet,
+				Type:           TypeIPv4,
+				Address:        "alice.example.org",
+			},
+			Name: "Example",
+			Connection: &gosdp.Connection{
+				Network: NetworkInternet,
+				Type:    TypeIPv4,
+				Address: "127.0.0.1",
+			},
+			Media: []*gosdp.Media{
+				{
+					Type:  "audio",
+					Port:  10000,
+					Proto: "RTP/AVP",
+					Format: []*gosdp.Format{
+						{Payload: 0, Name: "PCMU", ClockRate: 8000},
+						{Payload: 8, Name: "PCMA", ClockRate: 8000},
+					},
+				},
+			},
+			Attributes: []*gosdp.Attr{
+				{Name: "recvonly"},
+			},
+		},
+	},
+	{
+		Name: "SCTP Example",
+		Data: `v=0
+o=- 0 2 IN IP4 127.0.0.1
+s=-
+c=IN IP4 127.0.0.1
+t=0 0
+m=application 10000 DTLS/SCTP 5000
+a=sctpmap:5000 webrtc-datachannel 256
+m=application 10000 UDP/DTLS/SCTP webrtc-datachannel
+a=sctp-port:5000
+`,
+		Session: &gosdp.Session{
+			Origin: &gosdp.Origin{
+				Username:       "-",
+				SessionID:      0,
+				SessionVersion: 2,
+				Network:        NetworkInternet,
+				Type:           TypeIPv4,
+				Address:        "127.0.0.1",
+			},
+			Name: "-",
+			Connection: &gosdp.Connection{
+				Network: NetworkInternet,
+				Type:    TypeIPv4,
+				Address: "127.0.0.1",
+			},
+			Media: []*gosdp.Media{
+				{
+					Type:        "application",
+					Port:        10000,
+					Proto:       "DTLS/SCTP",
+					FormatDescr: "5000",
+					Attributes: gosdp.Attributes{
+						{"sctpmap", "5000 webrtc-datachannel 256"},
+					},
+				},
+				{
+					Type:        "application",
+					Port:        10000,
+					Proto:       "UDP/DTLS/SCTP",
+					FormatDescr: "webrtc-datachannel",
+					Attributes: gosdp.Attributes{
+						{"sctp-port", "5000"},
+					},
+				},
+			},
+		},
+	},
+}
+
+func BenchmarkEncode(b *testing.B) {
+	for _, test := range marshalTests {
+		b.Run(test.Name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				var res string
+				buf := bytes.NewBufferString((res))
+
+				e := NewEncoder(buf)
+				e.Encode(test.Session)
+			}
+		})
+	}
+}
+
+func BenchmarkDefaultEncode(b *testing.B) {
+	for _, test := range defaultMarshalTests {
+		b.Run(test.Name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				var res string
+				buf := bytes.NewBufferString((res))
+
+				e := gosdp.NewEncoder(buf)
+				e.Encode(test.Session)
+			}
+		})
+	}
 }
